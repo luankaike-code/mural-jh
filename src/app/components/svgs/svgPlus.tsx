4e959c11b6a875d6212445439d9154f2f2987741
luankaike-code <luankaikemacksantos@gmail.com>
2025-06-27 15:36:51 -0300

    descartei a ideia de svg animado, por enquanto

--- src/app/components/svgs/svgPlus.tsx
+++ src/app/components/svgs/svgPlus.tsx
@@ -1,13 +1,10 @@
 import { HTMLAttributes } from "react";
 
-export interface IPropsSvgPlus extends HTMLAttributes<HTMLOrSVGElement> {
-	isOpen?: boolean
-}
+export interface IPropsSvgPlus extends HTMLAttributes<HTMLOrSVGElement> {}
 
-export function SvgPlus({isOpen, ...rest}: IPropsSvgPlus){
-	console.log(isOpen)
+export function SvgPlus({props}: IPropsSvgPlus){
 	return(
-		<svg width="100" height="100" viewBox="0 0 100 100" {...rest}>
+		<svg width="100" height="100" viewBox="0 0 100 100" {...props}>
 			<rect x="45" y="20" width="10" height="60" />
 			<rect x="20" y="45" width="60" height="10" />
 		</svg>

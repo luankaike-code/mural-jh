5bd252bd0368e4aa13e7aa30f88bf3b5217b50e0
luankaike-code <luankaikemacksantos@gmail.com>
2025-07-01 16:58:11 -0300

    Agora aceitam ref

--- src/app/components/conteiner/conteiner.tsx
+++ src/app/components/conteiner/conteiner.tsx
@@ -1,12 +1,13 @@
 import { HTMLAttributes } from "react"
 import { twMerge } from "tailwind-merge"
+import { forwardRef } from "react"
 
 export interface IPropsConteiner extends HTMLAttributes<HTMLDivElement> {}
 
-export function Conteiner({children, className, ...rest}: IPropsConteiner){
+export const Conteiner = forwardRef<HTMLDivElement, IPropsConteiner>(({children, className, ...rest}, ref) => {
 	return(
-		<div className={twMerge("bg-emphasis shadow-[-5px_5px] shadow-shadow", className)} {...rest}>
+		<div className={twMerge("bg-emphasis shadow-[-5px_5px] shadow-shadow", className)} ref={ref} {...rest}>
 			{children}
 		</div>
 	)
-}
+})
--- src/app/components/post/post.tsx
+++ src/app/components/post/post.tsx
@@ -1,15 +1,16 @@
 import { Conteiner, IPropsConteiner, PostBody, PostHeader, PostTagConteiner } from "@components"
 import { twMerge } from "tailwind-merge"
 import { IDataPost } from "@/helpers/interfaces"
+import { forwardRef } from "react"
 
 export interface IPropsPost extends IDataPost, IPropsConteiner {}
 
-export function Post({codename, markdown, className, tags, ...rest}: IPropsPost){
+export const Post = forwardRef<HTMLDivElement, IPropsPost>(({codename, markdown, className, tags, ...rest}, ref) => {
 	return(
-		<Conteiner className={twMerge("flex flex-col gap-4 p-4 duration-200 hover:shadow-[-12px_12px]", className)} {...rest}>
+		<Conteiner className={twMerge("flex flex-col gap-4 p-4 duration-200 hover:shadow-[-12px_12px]", className)} ref={ref} {...rest}>
 			<PostHeader className="break-all">{codename}</PostHeader>
 			<PostTagConteiner tags={tags} />
 			<PostBody markdown={markdown}/>
 		</Conteiner>
 	)
-}
+})
